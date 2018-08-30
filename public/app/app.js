bfe19d7267597c34d669adca00968f60464f10ed
mattfro333 <mattfro333@gmail.com>
2018-08-30 00:37:30 -0600

    angular var finally works with jade

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when('home', {url: '/', templateUrl: '/partials/main', controller: 'mainCtrl'})
+      .when('', {templateUrl: '/partials/main', controller: 'mainCtrl'})
 });
 
 angular.module('app').controller('mainCtrl', function($scope){
