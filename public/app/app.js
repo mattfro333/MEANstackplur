1bede4a6601b3d12a755c41b752c96464d5a47dc
mattfro333 <mattfro333@gmail.com>
2018-08-30 23:45:32 -0600

    trying to add a cart function

--- public/app/app.js
+++ public/app/app.js
@@ -9,4 +9,17 @@ angular.module('app').config(function($routeProvider, $locationProvider){
 angular.module('app').controller('mainCtrl', function($scope){
   console.log("where's?")
     $scope.myVar = "hello angular";
+    $scope.products = productService.getProducts();
+
+$scope.addToCart = function(product) {
+  cartService.addToCart(product).then(function(){
+  cartService.getCart().then(function(res){
+    $scope.cart = res.data;
+  })
+ })
+}
+
+cartService.getCart().then(function(res){
+  $scope.cart = res.data;
+})
 });
