e9d1f5d8ac8f82b0550b945b0153e7b2f4979dd8
mattfro333 <mattfro333@gmail.com>
2018-09-18 02:03:47 -0600

    made the cart o bit better

--- public/app/app.js
+++ public/app/app.js
@@ -10,25 +10,25 @@ angular.module('app').controller('mainCtrl', function($scope){
   console.log("where's?")
     $scope.myVar = "hello angular";
 
-    // function cart($scope) {
+
      $scope.items = [{
                 qty: 10,
                 description: 'item',
                 cost: 9.95
               }]
-    //
-    //     $scope.addItem = function() {
-    //         $scope.invoice.items.push({
-    //             qty: 1,
-    //             description: '',
-    //             cost: 0
-    //         });
-    //     },
-    //
-    //     $scope.removeItem = function(index) {
-    //         $scope.invoice.items.splice(index, 1);
-    //     },
-    //
+
+        $scope.addItem = function() {
+            $scope.items.push([{
+                qty: 1,
+                description: '',
+                cost: 0
+            }]);
+        },
+
+        $scope.removeItem = function(index) {
+            $scope.items.splice(index, 1);
+        },
+
         $scope.total = function() {
             var total = 0;
             angular.forEach($scope.items, function(item) {
