e4071b65155583f2dc1e71b2d051e606dd1a4d4d
mattfro333 <mattfro333@gmail.com>
2019-06-20 21:49:18 -0600

    I attempted to switch the jade to index.html but the error in the console said thml module not found

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when('', {templateUrl: '/partials/main', controller: 'mainCtrl'})
+      .when('', {templateUrl: '.././index.html', controller: 'mainCtrl'})
 });
 
 angular.module('app').controller('mainCtrl', function($scope){
